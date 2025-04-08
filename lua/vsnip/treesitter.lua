0a4b8419e44f47c57eec4c90df17567ad4b1b36e
Dmytro Z. <zdm@softvisio.net>
2025-04-08 17:31:55 +0300

    Refactor treesitter filetype detection (#281)
    * feat: refactor treesitter filetype detection
    
    * feat: refactor treesitter filetype detection
    
    * feat: refactor treesitter filetype detection
    
    * feat: refactor treesitter filetype detection
    
    * feat: refactor treesitter filetype detection
    
    * feat: refactor treesitter filetype detection
    
    * chore: update docs
    
    * chore: check filetype separator to '.'
    
    * chore: revert filetype separator to '/'
    
    * chore: revert filetype separator to '/'

--- lua/vsnip/treesitter.lua
+++ lua/vsnip/treesitter.lua
@@ -10,25 +10,56 @@ if not ok_utils then
   ts_utils = nil
 end
 
-function M.is_available ()
+local function get_parser_filetype ( lang )
+  if lang and ts_parsers.list[ lang ] then
+    return ts_parsers.list[ lang ].filetype or lang
+  else
+    return ""
+  end
+end
+
+local function is_available ()
   return ok_parsers and ok_utils
 end
 
 function M.get_ft_at_cursor ( bufnr )
-  if M.is_available() then
+  local filetypes = {
+    filetype = "",
+    injected_filetype = "",
+  }
+
+  if is_available() then
     local cur_node = ts_utils.get_node_at_cursor( vim.fn.bufwinid( bufnr ) )
 
     if cur_node then
       local parser = ts_parsers.get_parser( bufnr )
-      local lang = parser:language_for_range( { cur_node:range() } ):lang()
+      local language_tree_at_cursor = parser:language_for_range( { cur_node:range() } )
+      local language_at_cursor = language_tree_at_cursor:lang()
 
-      if ts_parsers.list[ lang ] ~= nil then
-        return ts_parsers.list[ lang ].filetype or lang
+      local filetype = get_parser_filetype( language_at_cursor )
+
+      if filetype ~= "" then
+        filetypes.filetype = filetype
+
+        local parent_language_tree = language_tree_at_cursor:parent()
+
+        if parent_language_tree then
+          local parent_language = parent_language_tree:lang()
+          local parent_filetype = get_parser_filetype( parent_language )
+
+          if parent_filetype ~= "" then
+            filetypes.injected_filetype = parent_filetype .. "/" .. filetype
+          end
+        end
+
+        return filetypes
       end
     end
   end
 
-  return vim.bo[ bufnr ].filetype or ""
+  filetypes.filetype = vim.bo[ bufnr ].filetype or ""
+
+  return filetypes
 end
 
 return M
